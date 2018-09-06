ec3ed05755710b70ee70cab569b4522b79428afc
johnstonstu <stu.w.johnston@gmail.com>
2018-09-06 15:03:14 -0700

    fix double sec folder

--- js/config/api.js
+++ js/config/api.js
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'https://api.graph.cool/simple/v1/cjlpns23e1idm0145rd6jqyck',
+  uri: 'https://api.graph.cool/simple/v1/cjlqx0eve5k0f0178zmdjjk82',
 });
 
 export default client;
