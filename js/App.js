bdd8de3e0e6255e85835c75dab13ec35ca1c899d
Sid Parmar <sidparmar@Sids-MacBook-Pro-2.local>
2018-09-07 09:45:41 -0700

    sid - resolved a nav bug from app.js

--- js/App.js
+++ js/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import RootStackNavigator from './navigation/RootStackNavigation';
+import SplashScreen from 'react-native-splash-screen';
 
 export default class App extends Component {
+
+  componentDidMount() {
+    SplashScreen.hide()
+  }
+  
   render() {
     return <RootStackNavigator />;
   }
